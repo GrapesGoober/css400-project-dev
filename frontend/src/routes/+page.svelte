73aec78adb9fc1fce49bb9b2e4e4c25486c6cf66
Graps <twinklierre@gmail.com>
2024-09-20 21:46:42 +0700

    #1 added basic wall detection

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -3,17 +3,26 @@
 	import Mallet from './mallet.svelte';
 
 	let mallet_pos = [50, 50]
+	let width, height
 
 </script>
 
 <!-- svelte-ignore a11y-no-static-element-interactions -->
-<svg on:mousemove={(e) => mallet_pos = [e.clientX, e.clientY]}>
-	<Mallet bind:move_to={mallet_pos}/>
-	<Puck/>
-</svg>
+<div bind:clientWidth={width} bind:clientHeight={height}>
+	<svg on:mousemove={(e) => mallet_pos = [e.clientX, e.clientY]}>
+		<Mallet bind:move_to={mallet_pos}/>
+		<Puck bind:width={width} bind:height={height}/>
+	</svg>
+</div>
+
+
 
 <style>
 	svg {
+		width: 100%;
+		height: 100%;
+	}
+	div {
 		width: 100%;
 		height: 80vh;
 		margin: -8px;
--- frontend/src/routes/puck.svelte
+++ frontend/src/routes/puck.svelte
@@ -2,11 +2,13 @@
     import { tweened } from 'svelte/motion'
 	import { linear } from 'svelte/easing'
 
+	export let width, height
+
 	const DELTA_TIME = 200
     let velocity = [50, 10]
 
     let puck_coords = tweened(
-		[50, 50],
+		[0, 0],
 		{ duration: DELTA_TIME, easing: linear }
 	)
 
@@ -17,6 +19,15 @@
 		])
     }, DELTA_TIME)
 
+	// this wall detection isn't reliable due to high DELTA_TIME
+	// however, the reactive check is as fast as the browser suppport, 
+	// so the solution lies somewhere in setInterval and tweening
+	$: if ($puck_coords[0] > width) {
+		velocity[0] = -velocity[0]
+	}
+	$: if ($puck_coords[1] < 0 || $puck_coords[1] > height) {
+		velocity[1] = -velocity[1]
+	}
 </script>
 
 <circle cx={$puck_coords[0]} cy={$puck_coords[1]} r=10/>
