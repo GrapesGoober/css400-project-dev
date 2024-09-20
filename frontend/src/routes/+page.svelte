2ce46fa2738e1c5cd11e375ad664d92c8751bef9
Graps <twinklierre@gmail.com>
2024-09-20 20:20:55 +0700

    #1 added basic drag controls

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,29 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import { tweened } from 'svelte/motion'
+	import { cubicOut } from 'svelte/easing'
+
+    let coords = tweened(
+		{ x: 50, y: 50 },
+		{
+			duration: 200,
+			easing: cubicOut
+		}
+	)
+
+</script>
+
+<!-- svelte-ignore a11y-no-static-element-interactions -->
+<svg on:mousemove={(e) => coords.set({ x: e.clientX, y: e.clientY })}>
+	<circle cx={$coords.x} cy={$coords.y} r=10/>
+</svg>
+
+<style>
+	svg {
+		width: 100%;
+		height: 80vh;
+		margin: -8px;
+	}
+	circle {
+		fill: #ff3e00;
+	}
+</style>
