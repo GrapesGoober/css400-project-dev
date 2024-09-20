a0211977a76baeb1f4e713ff0fb94f2888c43952
Graps <twinklierre@gmail.com>
2024-09-20 21:07:38 +0700

    #1 added puck tweened velocity

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,26 +1,34 @@
 <script>
     import { tweened } from 'svelte/motion'
-	import { cubicOut } from 'svelte/easing'
+	import { cubicOut, linear } from 'svelte/easing'
 
     let coords = tweened(
 		{ x: 50, y: 50 },
 		{ duration: 200, easing: cubicOut }
 	)
 
-    let puck_coords = { x: 100, y: 100 }
-    let velocity = { x: 10, y: 0 }
+    let velocity = { x: 50, y: 10 }
+
+	const DELTA_TIME = 200
+
+    let puck_coords = tweened(
+		{ x: 100, y: 100 },
+		{ duration: DELTA_TIME, easing: linear }
+	)
 
     setInterval(() => {
-        puck_coords.x += velocity.x
-        puck_coords.y += velocity.y
-    }, 1000)
+        puck_coords.set({
+			x: $puck_coords.x + velocity.x,
+			y: $puck_coords.y + velocity.y
+		})
+    }, DELTA_TIME)
 
 </script>
 
 <!-- svelte-ignore a11y-no-static-element-interactions -->
 <svg on:mousemove={(e) => coords.set({ x: e.clientX, y: e.clientY })}>
 	<circle cx={$coords.x} cy={$coords.y} r=20/>
-	<circle cx={puck_coords.x} cy={puck_coords.y} r=10/>
+	<circle cx={$puck_coords.x} cy={$puck_coords.y} r=10/>
 </svg>
 
 <style>
