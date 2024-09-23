167dd2657cf8c28293063d81a596415621913a81
Graps <twinklierre@gmail.com>
2024-09-23 10:13:53 +0700

    removed tweening

--- frontend/src/routes/puck.svelte
+++ frontend/src/routes/puck.svelte
@@ -6,32 +6,26 @@
 
 	export let width, height
 
-	const DELTA_TIME = 200
-    let velocity = [50, 10]
+	const DELTA_TIME = 10
+    let velocity = [5, 1]
 
-    let puck_coords = tweened(
-		[0, 0],
-		{ duration: DELTA_TIME, easing: linear }
-	)
+    let puck_coords = [0, 0]
 
     setInterval(() => {
-		let new_coords = cop($puck_coords)
-		add(new_coords, velocity)
-        puck_coords.set(new_coords)
+		add(puck_coords, velocity)
+		puck_coords = puck_coords
     }, DELTA_TIME)
 
-	// this wall detection isn't reliable due to high DELTA_TIME
-	// however, the reactive check is as fast as the browser suppport, 
-	// so the solution lies somewhere in setInterval and tweening
-	$: if ($puck_coords[0] > width) {
+	$: if (puck_coords[0] > width) {
+		console.log("hit")
 		velocity[0] = -velocity[0]
 	}
-	$: if ($puck_coords[1] < 0 || $puck_coords[1] > height) {
+	$: if (puck_coords[1] < 0 || puck_coords[1] > height) {
 		velocity[1] = -velocity[1]
 	}
 </script>
 
-<circle cx={$puck_coords[0]} cy={$puck_coords[1]} r=10/>
+<circle cx={puck_coords[0]} cy={puck_coords[1]} r=10/>
 
 <style>
 	circle {
