fb3f32caec3edacde8ad04eeca62ebfe0842ebe2
grapsgoober <twinklierre@gmail.com>
2024-10-30 11:17:23 +0700

    CSS400-10 refactored to monolithic OOP architecture

--- frontend/src/lib/index.ts
+++ frontend/src/lib/index.ts
@@ -1,1 +1,60 @@
 // place files you want to import through the `$lib` alias in this folder.
+import { Vector } from 'vecti';
+
+export class Circle {
+    position: Vector = new Vector(0, 0);
+    velocity: Vector = new Vector(0, 0);
+    radius: number = 15;
+
+    update_position(dt: number) {
+        // update its position using velocity
+        this.position = this.velocity
+            .multiply(dt)
+            .add(this.position);
+    }
+}
+
+export class Mallet extends Circle {
+    // accellerate to a new position with adjustable delta_v coef
+    // higher the COEF, faster the mallet react to mouse
+    accelerate_towards(to: Vector) {
+        const COEF = 3;
+        let delta_v: Vector = to.subtract(this.position);
+        this.velocity = delta_v.multiply(COEF);
+    }
+}
+
+export class Puck extends Circle {
+    // detect that it collides with mallet,
+    // and adjust its position and velocity accordingly
+    resolve_mallet_collision(other: Circle) {
+        let delta_pos: Vector = this.position.subtract(other.position);
+        let normal: Vector = delta_pos.normalize()
+		if (delta_pos.length() < this.radius + other.radius) {
+			// calculate adjustment vector to move puck away from mallet
+			let adj: Vector = normal
+                .multiply(this.radius + other.radius)
+                .subtract(delta_pos);
+			this.position = this.position.add(adj);
+			// calculate delta velocity to apply 
+            // FIXME: Post-collision velocity bug
+            // - the collision resolution is not realistic
+            // - has a "sticky" feel since it's just updating its velocity
+			let delta_v = other.velocity
+                .subtract(this.velocity)
+                .multiply(0.5);
+			this.velocity = this.velocity.add(delta_v)
+		}
+    }
+
+    // detect that it collides with wall,
+    // and adjust its position and velocity accordingly
+    resolve_wall_collision(width: number, height: number) {
+        if (this.position.x > width || this.position.x < 0) {
+            this.velocity = this.velocity.hadamard(new Vector(-1, 1))
+        }
+        if (this.position.y < 0 || this.position.y > height) {
+            this.velocity = this.velocity.hadamard(new Vector(1, -1))
+        }
+    }
+}
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,27 +1,56 @@
-<script>
-	import Puck from './puck.svelte';
-	import Mallet from './mallet.svelte';
+<script lang="ts">
+    import { Mallet, Puck } from '$lib';
+    import { onMount } from 'svelte';
 	import { Vector } from 'vecti'
 	
-	let mouse_pos = new Vector(50, 50)
-	let width, height
-	let mallet_pos = new Vector(0, 0)
-	let mallet_velocity = new Vector(0, 0)
+	let width: number, height: number;
+	let mousePos = new Vector(0, 0);
+
+	let mallet = new Mallet();
+	let puck = new Puck();
+	puck.position = new Vector(100,100);
+
+	let prev_timestamp: number = 0
+	function update(timestamp: number) {
+		requestAnimationFrame(update);
+		let dt: number = (timestamp - prev_timestamp) / 1000;
+		prev_timestamp = timestamp;
+
+		// control mallet
+		mallet.accelerate_towards(mousePos);
+
+		// have puck check collision
+		puck.resolve_wall_collision(width, height);
+		puck.resolve_mallet_collision(mallet);
+
+		// update their positions
+		mallet.update_position(dt);
+		puck.update_position(dt);
+		
+		// force svelte to recognize changes
+		mallet = mallet;
+		puck = puck;
+	}
+
+	onMount(() => {
+		requestAnimationFrame(update);
+	});
 
 </script>
 
 <!-- I believe there's a bind hell going on here... Svelte store? -->
 <!-- svelte-ignore a11y-no-static-element-interactions -->
 <div bind:clientWidth={width} bind:clientHeight={height}>
-	<svg on:mousemove={(e) => mouse_pos = new Vector(e.clientX, e.clientY)}>
-		<Mallet bind:move_to={mouse_pos} bind:coords={mallet_pos} bind:velocity={mallet_velocity}/>
-		<Puck bind:width={width} bind:height={height} bind:mallet_pos={mallet_pos} bind:mallet_velocity={mallet_velocity}/>
+	<svg on:mousemove={(e) => mousePos = new Vector(e.clientX, e.clientY)}>
+		<circle cx={mallet.position.x} cy={mallet.position.y} r={mallet.radius}/>
+		<circle cx={puck.position.x} cy={puck.position.y} r={puck.radius}/>
 	</svg>
 </div>
 
-
-
 <style>
+	circle {
+		fill: #ff3e00;
+	}
 	svg {
 		width: 100%;
 		height: 100%;
