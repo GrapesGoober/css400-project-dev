3eb2a111f96537a7333d46c8299027cf597e80d1
Graps <twinklierre@gmail.com>
2024-12-01 14:32:08 +0700

    CSS400-13 add file download API

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,10 @@
 import csv
 from fastapi import FastAPI
+from fastapi.responses import FileResponse
 from pydantic import BaseModel
 
 app = FastAPI()
+dataset_file = "./backend/dataset.csv"
 
 class GamestateRecord(BaseModel):
     puck_x: float
@@ -16,6 +18,16 @@ class GamestateRecord(BaseModel):
 
 @app.post("/api/record/")
 async def record(record: GamestateRecord):
-    with open("./backend/dataset.csv", "a", newline='') as f:
+    with open(dataset_file, "a", newline='') as f:
         writer = csv.writer(f)
         writer.writerow(record.model_dump().values())
+
+@app.get("/api/download")
+async def clear_file():
+    resp = FileResponse(dataset_file, media_type="text/csv", filename="dataset.csv")
+    return resp
+
+@app.get("/api/clear")
+async def clear_file():
+    open(dataset_file, 'w').close() # Clear existing data
+    return
